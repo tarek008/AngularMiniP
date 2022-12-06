1baa1a9befebdb98ab322bae208f9263d3048ec5
tarek008 <tarek.ayadi@esprit.tn>
2022-12-06 01:37:03 +0100

    new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  urlG: 'http://localhost:8089/SpringMVC',
+  url: 'http://localhost:8089/SpringMVC/controllerEtudiant/',
+  urlDep: 'http://localhost:8089/SpringMVC/DepartmentController/',
+  urlEns: 'http://localhost:8089/SpringMVC/EnseignantController/',
+  urlCont: 'http://localhost:8089/SpringMVC'
 };
